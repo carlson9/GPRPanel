b4ddce75d21711600953dad196e776addb4a347e
carlson9 <carlson.david@wustl.edu>
2018-06-18 15:19:53 +0300

    combined n.cores arguments

--- GPRPanel.R
+++ GPRPanel.R
@@ -1,6 +1,6 @@
 GPRPanel = function(form, group, time, data,
         loglik = FALSE, seed = 1919,
-        cores = 'max', n.cores = NULL, auto_write = TRUE,
+        n.cores = 'max', auto_write = TRUE,
         iter = 1000, chains = 4, refresh = 100){
     g = model.matrix(~ group - 1, data)
     names = as.character(attr(terms(form), 'variables'))[-1]
@@ -8,7 +8,7 @@ GPRPanel = function(form, group, time, data,
     X = data[,names[2:length(names)]]
     X = cbind(as.matrix(X), g)
     requireNamespace(rstan)
-    if(cores == 'max') options(mc.cores = parellel::detectCores())
+    if(n.cores == 'max') options(mc.cores = parellel::detectCores())
     else options(mc.cores = n.cores)
     if(auto_write) rstan_options(auto_write = TRUE)
     t = scale(data[,time])
