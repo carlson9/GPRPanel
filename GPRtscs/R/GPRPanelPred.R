5bf47e7afe7ca8c9bc6dd0e834440d10fccfc875
Dilara Karaduman <55814565+karadumandilara@users.noreply.github.com>
2022-04-28 21:10:48 +0300

    Update GPRPanelPred.R

--- GPRtscs/R/GPRPanelPred.R
+++ GPRtscs/R/GPRPanelPred.R
@@ -1,3 +1,25 @@
+#' @description
+#' Fit a model defined in the Stan model and return the fitted
+#' returns the fitted model.
+#'
+#' @param form An object of class "formula" (or one that can be coerced to that class): a symbolic description of the model to be fitted
+#' @param group Column name in data containing names of units under study (e.g., countries)
+#' @param time Column name in data containing the temporal element (e.g., year)
+#' @param data A data.frame containing everything in form and the group and time columns
+#' @param Z Z value for normal distribution
+#' @param Z_corr z-scores for inference on parameters
+#' @param loglik Logical indicating whether or not to estimate the log-likelihood. Defaults to FALSE
+#' @param seed The seed for random number generation of the initial Stan parameter values. The default is 1919
+#' @param n.cores The number of cores to use when executing the Markov chains in parallel. The default is 'max' (i.e., all available cores)
+#' @param iter The number of iterations per chain. Default is 1000
+#' @param chains A positive integer specifying the number of Markov chains. The default is 4
+#' @param refresh Controls how often the progress of the sampling is reported
+#' @param auto_write A logical scalar (defaulting to TRUE) that controls whether a compiled instance of a stanmodel-class is written to the hard disk in the same directory.
+#' @param ... Additional arguments that are passed to sampling in rstan
+#'
+#' @details
+#'GPRPAnelPred function sets run-time options used to fit the model.
+#'
 GPRPanelPred = function(form, group, time, data, Z, Z_corr,
         seed = 1919,
         n.cores = 'max', auto_write = TRUE,
@@ -26,5 +48,6 @@ GPRPanelPred = function(form, group, time, data, Z, Z_corr,
             seed = seed, iter=iter, chains=chains, refresh = refresh)
     return(fit)
 }
+
 
 
